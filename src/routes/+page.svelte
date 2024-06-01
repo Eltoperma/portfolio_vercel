0fa9f2567ddeaa42ea3390a4e7af114a63a2855e
Eltoperma <91690410+Eltoperma@users.noreply.github.com>
2024-06-01 23:15:56 +0200

    impl

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import CalendarIcon from "svelte-radix/Calendar.svelte";
+    import {
+      DateFormatter,
+      type DateValue,
+      getLocalTimeZone
+    } from "@internationalized/date";
+    import { cn } from "$lib/utils.js";
+    import { Button } from "$lib/components/ui/button/index.js";
+    import { Calendar } from "$lib/components/ui/calendar/index.js";
+    import * as Popover from "$lib/components/ui/popover/index.js";
+   
+    const df = new DateFormatter("en-US", {
+      dateStyle: "long"
+    });
+   
+    let value: DateValue | undefined = undefined;
+  </script>
+   
+  <Popover.Root>
+    <Popover.Trigger asChild let:builder>
+      <Button
+        variant="outline"
+        class={cn(
+          "w-[240px] justify-start text-left font-normal",
+          !value && "text-muted-foreground"
+        )}
+        builders={[builder]}
+      >
+        <CalendarIcon class="mr-2 h-4 w-4" />
+        {value ? df.format(value.toDate(getLocalTimeZone())) : "Pick a date"}
+      </Button>
+    </Popover.Trigger>
+    <Popover.Content class="w-auto p-0" align="start">
+      <Calendar bind:value />
+    </Popover.Content>
+  </Popover.Root>
