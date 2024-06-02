999cbdc024e255e88b8661a6a6e34e8818a6fcc7
Eltoperma <91690410+Eltoperma@users.noreply.github.com>
2024-06-03 00:57:15 +0200

    impl

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,84 @@
 <script lang="ts">
-    import CalendarIcon from "svelte-radix/Calendar.svelte";
-    import {
-      DateFormatter,
-      type DateValue,
-      getLocalTimeZone
-    } from "@internationalized/date";
-    import { cn } from "$lib/utils.js";
-    import { Button } from "$lib/components/ui/button/index.js";
-    import { Calendar } from "$lib/components/ui/calendar/index.js";
-    import * as Popover from "$lib/components/ui/popover/index.js";
-   
-    const df = new DateFormatter("en-US", {
-      dateStyle: "long"
-    });
-   
-    let value: DateValue | undefined = undefined;
-  </script>
-   
-  <Popover.Root>
-    <Popover.Trigger asChild let:builder>
-      <Button
-        variant="outline"
-        class={cn(
-          "w-[240px] justify-start text-left font-normal",
-          !value && "text-muted-foreground"
-        )}
-        builders={[builder]}
-      >
-        <CalendarIcon class="mr-2 h-4 w-4" />
-        {value ? df.format(value.toDate(getLocalTimeZone())) : "Pick a date"}
-      </Button>
-    </Popover.Trigger>
-    <Popover.Content class="w-auto p-0" align="start">
-      <Calendar bind:value />
-    </Popover.Content>
-  </Popover.Root>
+	import Separator from './../lib/components/ui/separator/separator.svelte';
+	import * as Avatar from "$lib/components/ui/avatar/index.js";
+  import * as Accordion from "$lib/components/ui/accordion/index.js";
+	import { Button } from '$lib/components/ui/button';
+  </script>    
+    <div class="p-4 flex flex-row justify-between m-auto  max-h-[600px] max-w-[800px]">
+      <div class="w-1/2 from-slate-400 to-transparent" >
+        <h2 class="font-semibold text-2xl">Wer bin ich?</h2>
+        <p class="whitespace-normal pt-4">Ich bin Tjark Hüter, gebürtiger Kieler und absolviere zurzeit mein Duales Studium zum Wirtschaftsinformatiker. Nebst erfahrungen von der Arbeit bei freenet DLS suche ich noch Erfahrungen in Hobbyprojekten, sei es Programmieren, Film, Ton, Schnitt oder einfach mal ne Runde fahrradfahren.</p>
+      </div>
+      <div class="w-1/2 min-h-[160px] min-w-[160px]">
+        <Avatar.Root>
+          <Avatar.Image  src="pfp.jpg" alt="@Tjark Hüter" />
+          <Avatar.Fallback>Tjark Hüter</Avatar.Fallback>
+        </Avatar.Root>
+      </div>
+    </div>
+    <Separator class=" w-1/3 my-8 mx-auto" />
+    <div class="m-auto max-w-[800px] p-4 dark:bg-slate-900 shadow-2xl rounded-md">
+      <h3 class="text-xl">Skills</h3>
+      <Accordion.Root class="w-full sm:max-w-[70%]">
+        <Accordion.Item value="item-1">
+          <Accordion.Trigger>Sprachen</Accordion.Trigger>
+          <Accordion.Content
+          ><ul>
+            <li><img src="icons/brit.svg" alt="- ">Englisch</li>
+            <li><img src="icons/c.svg" alt="- ">C#</li>
+            <li><img src="icons/java.svg" alt="- ">Java</li>
+            <li><img src="icons/html.svg" alt="- ">HTML</li>
+            <li><img src="icons/css.svg" alt="- ">CSS</li>
+            <li><img src="icons/js.svg" alt="- ">Javascript</li>
+            <li><img src="icons/typescript.svg" alt="- ">Typescript</li>
+            <li><img src="icons/pyth.svg" alt="- ">python</li>
+          </ul></Accordion.Content
+          >
+        </Accordion.Item>
+        <Accordion.Item value="item-2">
+          <Accordion.Trigger>Frameworks</Accordion.Trigger>
+          <Accordion.Content>
+            <ul>
+              <li><img src="icons/svelte.svg" alt="- ">Svelte/Kit</li>
+              <li><img src="icons/prisma.svg" alt="- ">Prisma</li>
+              <li><img src="icons/tailwind.svg" alt="- ">Tailwind</li>
+              <li><img src="icons/brit.svg" alt="- ">Wordpress</li>
+              <li><img src="icons/net.svg" alt="- ">.NET</li>
+              <li><img src="icons/flutter.svg" alt="- ">Flutter</li>
+            </ul>
+          </Accordion.Content>
+        </Accordion.Item>
+        <Accordion.Item value="item-3">
+          <Accordion.Trigger>Fachentnisse</Accordion.Trigger>
+          <Accordion.Content>
+            <ul>
+              <li><img src="icons/photoshop.svg" alt="- ">Photoshop</li>
+              <li><img src="icons/photoshop.svg" alt="- ">Lightroom</li>
+              <li><img src="icons/indesign.svg" alt="- ">Indesign</li>
+              <li><img src="icons/illustrator.svg" alt="- ">Illustrator</li>
+              <li><img src="icons/cam.svg" alt="- ">Photographie</li>
+              <li><img src="icons/filmcam.svg" alt="- ">Videographie</li>
+              <li><img src="icons/micro.svg" alt="- ">Audiotechnik & Recording</li>
+              <li><img src="icons/davinci.svg" alt="- ">Ton-/Videoschnitt und nachbearbeitung</li>
+            </ul>
+          </Accordion.Content>
+        </Accordion.Item>
+      </Accordion.Root>
+    </div>
+    <div class="m-auto mt-8 max-w-[800px] p-4 rounded-md whitespace-normal">
+      <h3 class="text-xl mb-2">Interesse an Kontakt?</h3>
+      <p>Dann erreichen sie mich unter meiner E-Mail: <a class="underline text-blue-700" href="mailto:tjarkhueter@gmail.com">tjarkhueter@gmail.com</a> oder indem sie mir über das Kontaktformular eine Nachricht zukommen lassen.</p>
+      <div class="flex justify-center">
+        <Button class="m-auto mt-8" href="/Kontakt">Zum Kontaktformular</Button>
+      </div>
+    </div>
+    <div class="h-[50px]"></div>
+    
+      <style>
+        li img {
+          width: 1rem;
+          height: 1rem;
+          display: inline;
+          margin-right: 0.2rem;
+        }
+      </style>
